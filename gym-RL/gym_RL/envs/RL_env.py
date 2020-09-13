57f93bbfc6fa064c04dfefb17edfe50552ab1738
devpramod <pai.pra@husky.neu.edu>
2020-09-13 13:04:12 -0400

    updated env name

--- gym-RL/gym_RL/envs/RL_env.py
+++ gym-RL/gym_RL/envs/RL_env.py
@@ -3,54 +3,114 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 class RLEnv(gym.Env):
+    """Custom environment for stock market data"""
+    metadata = {'render.modes': ['human']}
 
-
-  def __init__(self,history_t=90):
-#     self.data = data
-    self.history_t = history_t
-    self.reset()
-    
-  def load_data(self, data):
-    self.data = data
-  
-  def step(self, action):
-    reward = 0
+    def __init__(self, df):
+        super(StockTradingEnv, self).__init__()
+        self.df = df
+        self.reward_range = (0, MAX_ACCOUNT_BALANCE) 
         
-    # act = 0: stay, 1: buy, 2: sell
-    if act == 1:
-        self.positions.append(self.data.iloc[self.t, :]['Close'])
-    elif act == 2: # sell
-        if len(self.positions) == 0:
-            reward = -1
-        else:
-            profits = 0
-            for p in self.positions:
-                profits += (self.data.iloc[self.t, :]['Close'] - p)
-            reward += profits
-            self.profits += profits
-            self.positions = []
-    
-    # set next time
-    self.t += 1
-    self.position_value = 0
-    for p in self.positions:
-        self.position_value += (self.data.iloc[self.t, :]['Close'] - p)
-    self.history.pop(0)
-    self.history.append(self.data.iloc[self.t, :]['Close'] - self.data.iloc[(self.t-1), :]['Close'])
-    
-    # clipping reward
-    if reward > 0:
-        reward = 1
-    elif reward < 0:
-        reward = -1
-    
-    return [self.position_value] + self.history, reward, self.done # obs, reward, done
-  
-  def reset(self):
-    self.t = 0
-    self.done = False
-    self.profits = 0
-    self.positions = []
-    self.position_value = 0
-    self.history = [0 for _ in range(self.history_t)]
-    return [self.position_value] + self.history # obs
+        # Actions of the format Buy x%, Sell x%, Hold, etc.
+        self.action_space = spaces.Box(
+        low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
+        
+        # Prices contains the OHCL values for the last five prices
+        self.observation_space = spaces.Box(
+        low=0, high=1, shape=(6, 6), dtype=np.float16)
+
+    def reset(self):
+        # Reset the state of the environment to an initial state
+        self.balance = INITIAL_ACCOUNT_BALANCE
+        self.net_worth = INITIAL_ACCOUNT_BALANCE
+        self.max_net_worth = INITIAL_ACCOUNT_BALANCE
+        self.shares_held = 0
+        self.cost_basis = 0
+        self.total_shares_sold = 0
+        self.total_sales_value = 0
+        
+        # Set the current step to a random point within the data frame
+        self.current_step = random.randint(0, len(self.df.loc[:, 'Open'].values) - 6)
+        return self._next_observation()
+
+    def _next_observation(self):
+        # Get the data points for the last 5 days and scale to between 0-1
+        frame = np.array([
+        self.df.loc[self.current_step: self.current_step +
+                    5, 'Open'].values / MAX_SHARE_PRICE,
+        self.df.loc[self.current_step: self.current_step +
+                    5, 'High'].values / MAX_SHARE_PRICE,
+        self.df.loc[self.current_step: self.current_step +
+                    5, 'Low'].values / MAX_SHARE_PRICE,
+        self.df.loc[self.current_step: self.current_step +
+                    5, 'Close'].values / MAX_SHARE_PRICE,
+        self.df.loc[self.current_step: self.current_step +
+                    5, 'Volume'].values / MAX_NUM_SHARES,
+    ])
+        # Append additional data and scale each value to between 0-1
+        obs = np.append(frame, [[
+            self.balance / MAX_ACCOUNT_BALANCE,
+            self.max_net_worth / MAX_ACCOUNT_BALANCE,
+            self.shares_held / MAX_NUM_SHARES,
+            self.cost_basis / MAX_SHARE_PRICE,
+            self.total_shares_sold / MAX_NUM_SHARES,
+            self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE),
+        ]], axis=0)
+        
+        return obs
+
+    def step(self, action):
+        # Execute one time step within the environment
+        self._take_action(action)
+        self.current_step += 1
+        if self.current_step > len(self.df.loc[:, 'Open'].values) - 6:
+            self.current_step = 0
+        delay_modifier = (self.current_step / MAX_STEPS)
+        
+        reward = self.balance * delay_modifier
+        done = self.net_worth <= 0
+        obs = self._next_observation()
+        return obs, reward, done, {}  
+
+    def _take_action(self, action):
+        # Set the current price to a random price within the time step
+        current_price = random.uniform(
+            self.df.loc[self.current_step, "Open"],
+            self.df.loc[self.current_step, "Close"])
+        action_type = action[0]
+        amount = action[1]
+        if action_type < 1:
+            # Buy amount % of balance in shares
+            total_possible = self.balance / current_price
+            shares_bought = total_possible * amount
+            prev_cost = self.cost_basis * self.shares_held
+            additional_cost = shares_bought * current_price
+            self.balance -= additional_cost
+            self.cost_basis = (prev_cost + additional_cost) / 
+                                    (self.shares_held + shares_bought)
+            self.shares_held += shares_bought
+        elif actionType < 2:
+            # Sell amount % of shares held
+            shares_sold = self.shares_held * amount . 
+            self.balance += shares_sold * current_price
+            self.shares_held -= shares_sold
+            self.total_shares_sold += shares_sold
+            self.total_sales_value += shares_sold * current_price
+        self.netWorth = self.balance + self.shares_held * current_price
+        if self.net_worth > self.max_net_worth:
+            self.max_net_worth = net_worth
+        if self.shares_held == 0:
+            self.cost_basis = 0    
+
+    def render(self, mode='human', close=False):
+        # Render the environment to the screen
+        profit = self.net_worth - INITIAL_ACCOUNT_BALANCE
+        print(f'Step: {self.current_step}')
+        print(f'Balance: {self.balance}')
+        print(f'Shares held: {self.shares_held}
+                (Total sold: {self.total_shares_sold})')
+        print(f'Avg cost for held shares: {self.cost_basis}
+                (Total sales value: {self.total_sales_value})')
+        print(f'Net worth: {self.net_worth}
+                (Max net worth: {self.max_net_worth})')
+        print(f'Profit: {profit}')              
