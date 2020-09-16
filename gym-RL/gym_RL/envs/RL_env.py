dbb9e6b75185a7ac8b5717a8a79622e9f399cdcb
devpramod <60084015+devpramod@users.noreply.github.com>
2020-09-16 00:57:51 -0400

    Update RL_env.py

--- gym-RL/gym_RL/envs/RL_env.py
+++ gym-RL/gym_RL/envs/RL_env.py
@@ -18,8 +18,9 @@ class RLEnv(gym.Env):
         self.reward_range = (-1, 1) 
         
         # Actions of the format Buy x%, Sell x%, Hold, etc.
-        self.action_space = spaces.Box(
-        low=np.array([0]), high=np.array([2]), dtype=np.float16)
+#         self.action_space = spaces.Box(
+#         low=np.array([0]), high=np.array([2]), dtype=np.float16)
+        self.action_space = spaces.Discrete(3)
         
         # Prices contains the OHCL values for the last five prices
         self.observation_space = spaces.Box(
